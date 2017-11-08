5d5d29093bedd22e78a425af1eb9c604dfb1d2d1
dayrui <dayrui@gmail.com>
2017-11-08 10:50:21 +0800

    no commit message

--- diy/dayrui/controllers/admin/Application.php
+++ diy/dayrui/controllers/admin/Application.php
@@ -14,6 +14,7 @@ class Application extends M_Controller {
 		$this->template->assign('menu', $this->get_menu_v3(array(
 		    fc_lang('应用管理') => array('admin/application/index', 'cloud'),
             fc_lang('更新缓存') => array('admin/home/cache', 'refresh'),
+            
 		)));
 		$this->load->model('application_model');
     }
