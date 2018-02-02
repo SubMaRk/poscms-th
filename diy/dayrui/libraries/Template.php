1e73b596fb46e919515e1f9a1c2cf96bebbfb438
dayrui <dayrui@gmail.com>
2018-02-02 15:43:42 +0800

    修复无法下载文件

--- diy/dayrui/libraries/Template.php
+++ diy/dayrui/libraries/Template.php
@@ -130,11 +130,11 @@ class Template {
         $this->_filename = $_name;
 
         // 加载编译后的缓存文件
-        $file = $this->get_file_name($_name, $_dir);
+        $xxxfile = $this->get_file_name($_name, $_dir);
         if (defined('SYS_DEBUG') && SYS_DEBUG) {
-            echo "<!--当前页面的模板文件是：$file （本代码只在调试模式下显示）-->".PHP_EOL;
+            echo "<!--当前页面的模板文件是：$xxxfile （本代码只在调试模式下显示）-->".PHP_EOL;
         }
-        include $this->load_view_file($file);
+        include $this->load_view_file($xxxfile);
 
         // 消毁变量
         $this->_include_file = NULL;
